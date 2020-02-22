0ef34c9b4f6a86c9064fa33f78e97ac3cb36c9e8
Alexey Meshcheryakov <alexeymeshcheryakov68@gmail.com>
2020-02-22 23:49:05 +0300

    Drawer change

--- src/Drawer/Drawer.java
+++ src/Drawer/Drawer.java
@@ -4,46 +4,40 @@ import Game_World.Game_World;
 import java.lang.*;
 
 public class Drawer {
-    public Drawer(Game_World gm)
-    {
+    public Drawer(Game_World gm) {
         game_world = gm;
     }
 
-    public void Draw()
-    {
-        final int X_SIZE = 150, Y_SIZE = 40;
-
+    public void Draw() {
+        final int X_SIZE = 167, Y_SIZE = 39;
 
         char[][] screen = new char[Y_SIZE][X_SIZE];
-        for(int i = 0; i < Y_SIZE; i++)
-        {
-            for(int j = 0; j < X_SIZE; j++)
-            {
-                System.out.print(" ");
-            }
-            System.out.print("\n\r");
-        }
 
-        for(int i = 0; i < Y_SIZE; i++)
-        {
-            for(int j = 0; j < X_SIZE; j++)
-            {
+        for (int i = 0; i < Y_SIZE; ++i) {
+            for (int j = 0; j < X_SIZE; ++j) {
+                screen[i][j] = ' ';
+            }
+        }
+        System.out.print(Array_to_String(screen));
+
+        for(int i = 0; i < Y_SIZE; i++) {
+            for(int j = 0; j < X_SIZE; j++) {
                 screen[i][j] = '.';
             }
         }
-        String str = new String(screen[0]);
         screen[game_world.player.pos_y][game_world.player.pos_x] = '@';
 
-        for(int i = 0; i < Y_SIZE; i++)
-        {
-            //for(int j = 0; j < X_SIZE; j++)
-            //{
-                //System.out.print(screen[i][j]);
-                System.out.print(str);
-            //}
-            System.out.print("\n\r");
-        }
+        System.out.print(Array_to_String(screen));
     }
 
+    private StringBuilder Array_to_String(char[][] array) {
+        StringBuilder result = new StringBuilder();
+        for (char[] chars : array) {
+            result.append(chars);
+            result.append("\n\r");
+        }
+
+        return result;
+    }
     Game_World game_world;
 }
