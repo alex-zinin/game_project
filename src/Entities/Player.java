537cb9501141bd48a9bea65a773f887e48e66e0d
alexzinin <zininalexander@yandex.ru>
2020-02-22 23:47:23 +0300

    Enemies have been added, health is inside the entity.

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -1,9 +1,9 @@
 package Entities;
 
 public class Player extends Entity {
-    public Player(Integer x, Integer y, int health0)
+    public Player(Integer x, Integer y, int health1)
     {
-        health = health0;
+        health = health1;
         pos_x = x;
         pos_y = y;
     }
